2d87f2eeeeceac61f85056c022fe6f2af8e3038b
fish895623 <dan990429@gmail.com>
2022-04-05 23:25:51 +0900

    first init

---
+++ src/pages/hello/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Page } from "rakkasjs";
+
+const asdf: Page = () => {
+  return (
+    <main>
+      <p>Hello World</p>
+    </main>
+  );
+};
+
+export default asdf;
